9cbe12f93e0e4a7f388e61a31e670cf23d3c5c18
Matt Burton <matthew.a.burton@gmail.com>
2024-03-31 13:08:34 -0400

    Add loader around <Resume> to handle Paged.js issue

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -7,30 +7,11 @@ import SkillsSection from "components/skills/SkillsSection"
 import SummarySection from "components/summary/SummarySection"
 import { fetchResumeData, ResumeData } from "data"
 
-const Resume = (): React.ReactElement => {
-  const [data, setData] = useState<ResumeData>()
+type ResumeContentProps = {
+  data: ResumeData
+}
 
-  async function doFetch() {
-    const d = await fetchResumeData()
-    setData(d)
-  }
-
-  async function doPreview() {
-    await new Previewer().preview()
-  }
-
-  useLayoutEffect(() => {
-    doFetch()
-  }, [])
-
-  useLayoutEffect(() => {
-    if (data) doPreview()
-  }, [data])
-
-  if (!data) {
-    return <span></span>
-  }
-
+const ResumeContent = ({ data }: ResumeContentProps): React.ReactElement => {
   return (
     <>
       <HeaderSection contact={data.contact} />
@@ -42,6 +23,51 @@ const Resume = (): React.ReactElement => {
       <FooterSection />
     </>
   )
+}
+
+const Resume = (): React.ReactElement => {
+  const [loaded, setLoaded] = useState<boolean>(false)
+  const [data, setData] = useState<ResumeData>()
+
+  /**
+   * Executes the `fetchResumeData` function.
+   */
+  async function doFetch() {
+    const d = await fetchResumeData()
+    setData(d)
+  }
+
+  /**
+   * Invokes the Page.js in-browser previewer.
+   */
+  async function doPreview() {
+    await new Previewer().preview()
+  }
+
+  // Fetch resume data on component load
+  useLayoutEffect(() => {
+    doFetch()
+  }, [])
+
+  // Paged.js appears to need a brief moment for the DOM content to fully load
+  // (a separate issue from network load time). 0.5 sec appears to be sufficient
+  // based on testing/observation.
+  useLayoutEffect(() => {
+    if (data) setTimeout(() => setLoaded(true), 500)
+  }, [data])
+
+  // Invoke `doPreview` if the data has been loaded
+  useLayoutEffect(() => {
+    if (data && loaded) doPreview()
+  }, [data, loaded])
+
+  // Render the resume content if the data has been loaded
+  if (data && loaded) {
+    return <ResumeContent data={data} />
+  }
+
+  // Render a fallback component if the data is still loading
+  return <span>Fallback</span>
 }
 
 export default Resume
